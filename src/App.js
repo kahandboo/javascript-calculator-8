f1ffa84d1ffe1f9ae71f1b8aacb551690c15380d
감사감 <kokio02@pusan.ac.kr>
2025-10-15 16:23:07 +0900

    feat(input): 사용자의 입력을 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import * as readline from 'readline'; 
+
 class App {
-  async run() {}
+  getUserInput() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    return new Promise((resolve) => {
+      rl.question('덧셈할 문자열을 입력하세요. \n', (input) => {
+        rl.close();
+        resolve(input.trim());
+      });
+    });
+  }
+  async run() {
+    const userInputData = await this.getUserInput();
+
+  }
+
 }
 
 export default App;
