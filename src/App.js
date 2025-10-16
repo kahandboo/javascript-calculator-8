447db1918286e37a59460bc347308ee3a3feab6d
감사감 <kokio02@pusan.ac.kr>
2025-10-16 21:49:00 +0900

    feat(parser): 입력 문자열에서 커스텀 구분자를 찾는 기능 구현

--- src/App.js
+++ src/App.js
@@ -10,13 +10,49 @@ class App {
     return new Promise((resolve) => {
       rl.question('덧셈할 문자열을 입력하세요. \n', (input) => {
         rl.close();
-        resolve(input.trim());
+        if (input.trim().length == 0) resolve("0");
+        else resolve(input.trim());
       });
     });
   }
+
+  /**
+   * 입력 문자열에서 커스텀 구분자를 찾아낸다.
+   * 커스텀 구분자가 존재하지 않는 경우엔 원본 문자열을 그대로 반환한다.
+   */
+  parseCustomSeparator(userInputData) {
+    if (userInputData.length < 5) {
+      return {
+        separator: "",
+        string: userInputData
+      };
+    }
+
+    if (userInputData[0] !== '/' || userInputData[1] !== '/') {
+      this.handleUnexpectedInput(); 
+    }
+    
+    if (userInputData[3] !== '\\' || userInputData[4] !== 'n') {
+      this.handleUnexpectedInput(); 
+    }
+    
+    const customSeparator = userInputData[2];
+    const leftOver = userInputData.substring(5); 
+    
+    return {
+      separator: customSeparator,
+      string: leftOver
+    };
+  }
+
+  handleUnexpectedInput() {
+    console.log("[ERROR]");
+  }
+
   async run() {
     const userInputData = await this.getUserInput();
-
+    const parsedInputData = this.parseCustomSeparator(userInputData);
+    
   }
 
 }
